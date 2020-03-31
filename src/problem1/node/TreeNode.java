bede4129e2d58007458210cf8eb6c6c67994036c
ankitpandey1787 <49266082+ankitpandey1787@users.noreply.github.com>
2020-03-31 18:49:25 +0530

    Update TreeNode.java

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,38 @@
  */
 package problem1.node;
 
-public class TreeNode {
+
+public class TreeNode<E> {
+    private E data;
+    private TreeNode<E> leftChild;
+    private TreeNode<E> rightChild;
+
+    public TreeNode(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public TreeNode<E> getLeftChild() {
+        return leftChild;
+    }
+
+    public TreeNode<E> getRightChild() {
+        return rightChild;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public void setLeftChild(TreeNode<E> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setRightChild(TreeNode<E> rightChild) {
+        this.rightChild = rightChild;
+    }
+
 }
