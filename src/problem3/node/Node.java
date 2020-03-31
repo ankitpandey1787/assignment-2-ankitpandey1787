cca34cafd1ecec5b901eaeb4357e5a20b1e9e34b
ankitpandey1787 <49266082+ankitpandey1787@users.noreply.github.com>
2020-03-31 19:20:22 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,4 +1,4 @@
-/*
+7/*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
@@ -6,5 +6,33 @@
  */
 package problem3.node;
 
-public class Node {
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
 }
