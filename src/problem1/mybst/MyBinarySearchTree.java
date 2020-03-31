ae4ef53aa052ddfd343cc237f997a27d1d561741
ankitpandey1787 <49266082+ankitpandey1787@users.noreply.github.com>
2020-03-31 18:32:42 +0530

    Update MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,57 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
-public class MyBinarySearchTree {
+public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
+
+    private TreeNode<E> root;
+    public int numberOfLeftChild = 0;
+    private int maxLevel = 1;
+
+    public TreeNode<E> getRoot() 
+{
+        return root;
+    }
+
+    @Override
+    public boolean add(E data) 
+{
+        root = addRecursive(root, data);
+        return true;
+    }
+
+    private TreeNode<E> addRecursive(TreeNode<E> currentNode, E data)
+ {
+        if (currentNode == null) {
+            return new TreeNode<>(data);
+        }
+ else if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.setLeftChild(addRecursive(currentNode.getLeftChild(), data));
+        } 
+else if ((Integer) data > (Integer) currentNode.getData()){
+            currentNode.setRightChild(addRecursive(currentNode.getRightChild(), data));
+        }
+        return currentNode;
+    }
+private void printLeftChild(TreeNode<E> node, int level) {
+        if (node == null) {
+            return;
+        }
+        if (maxLevel < level) {
+            System.out.print(node.getData() + " ");
+        }
+        maxLevel = level;
+        if (node.getLeftChild() == null) {
+            numberOfLeftChild++;
+            maxLevel++;
+        }
+        printLeftChild(node.getLeftChild(), level + 1);
+        printLeftChild(node.getRightChild(), level + 1);
+    }
+    public void print() {
+        printLeftChild(root, 1);
+        System.out.println();
+    }
 }
