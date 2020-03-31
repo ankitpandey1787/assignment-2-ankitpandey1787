4d878a5b88cf308ca0a0f2d2f33c04521a29641a
ankitpandey1787 <49266082+ankitpandey1787@users.noreply.github.com>
2020-03-31 19:04:51 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue<Student> myPriorityQueue = new MyPriorityQueue<>();
+        myPriorityQueue.add(new Student(1, "gopal"));
+        myPriorityQueue.add(new Student(5, "krishna"));
+        myPriorityQueue.add(new Student(3, "gopal"));
+        myPriorityQueue.add(new Student(2, "raghav"));
+        System.out.println("Queue After Arranging according to ROLL NUMBER : ");
+        System.out.println(myPriorityQueue);
+    }
 }
+
